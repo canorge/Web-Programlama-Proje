85539db3f89ce4392b52fbf16a3b4047e5e36e72
canorge <canorge54@gmail.com>
2024-12-15 23:25:20 +0300

    İletişim sayfası , hakkımızda ve Hizmetler düzenleme

--- Web Proe deneme/WebDeneme2/WebDeneme2/Controllers/HomeController.cs
+++ Web Proe deneme/WebDeneme2/WebDeneme2/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace WebDeneme2.Controllers
             return View();
         }
 
+        public IActionResult Iletisim()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
