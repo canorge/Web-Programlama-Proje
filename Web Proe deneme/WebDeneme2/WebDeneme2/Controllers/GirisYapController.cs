49030696f0ca9856baac378215735db296d8fcca
canorge <canorge54@gmail.com>
2024-12-16 20:54:52 +0300

    Üye ol sayfası tasarımı ve hakkımızda düzenleme

---
+++ Web Proe deneme/WebDeneme2/WebDeneme2/Controllers/GirisYapController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebDeneme2.Controllers
+{
+    public class GirisYapController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web Proe deneme/WebDeneme2/WebDeneme2/Models/GirisViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebDeneme2.Models
+{
+    public class GirisViewModel
+    {
+        [Required(ErrorMessage = "E-posta adresi gereklidir.")]
+        [EmailAddress(ErrorMessage = "Geçerli bir e-posta adresi giriniz.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Şifre gereklidir.")]
+        [DataType(DataType.Password)]
+        public string? Sifre { get; set; }
+    }
+}
